7968fed13488cdf28d29f3ea61a72c8e5b73b243
SHER11F <sherifmo3333@gmail.com>
2024-12-01 12:51:30 +0200

    From-023-to-026

---
+++ JavaScript/Elzero-JS-Assignments/From-023-to-026/Assignment-01/main.js
@@ -0,0 +1,16 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(Math.ceil(99999.9)); // 100000
+console.log(Math.floor(100000.4)); // 100000
+console.log(Math.trunc(100000.5416)); // 100000
+console.log(parseInt("100000")); // 100000
+console.log(Math.pow(10, 5)); // 100000
+console.log(10 ** 5); // 100000
+console.log(Math.round(100000.1)); // 100000
+console.log(Math.round(99999.6)); // 100000
+console.log(Math.max(100000, 5000, 40000, 54565)); // 100000
+console.log(Number((100000.2).toFixed(0))); // 100000
